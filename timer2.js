abf44ec8fa82e55668f2b7c04c93dab8ac3b308c
jgoncalvesjr <jgoncalvesjr@gmail.com>
2020-12-12 12:08:14 -0500

    Refactor to autobeep

--- timer2.js
+++ timer2.js
@@ -11,9 +11,9 @@ stdin.setRawMode(true);
 stdin.setEncoding('utf8');
 
 stdin.on('data', (key) => {
-  if (key === '\u0062') {
-    process.stdout.write('\r\x07*beep* \n');
-  }
+  // if (key === '\u0062') {
+  //   process.stdout.write('\n\r\x07*beep* \n');
+  // }
   if (key === '\u0003') {
     process.stdout.write('\nThanks for your time, bye!\n');
     process.exit();
@@ -21,23 +21,26 @@ stdin.on('data', (key) => {
 });
 
 const countdown = (timer) => {
-  if (isNaN(timer) || timer < 1 || timer > 9) {
+  if (timer.match(/[b]/i)) {
+    process.stdout.write('\r\x07*beep* \n');
+    askCountdown("Set a timer from 1 to 9: ");
+  } else if (isNaN(timer) || timer < 1 || timer > 9) {
     console.log(`Not a valid timer, try again!\n`);
     askCountdown("Set a timer from 1 to 9: ");
-  } else {
-    console.log(`setting timer for ${timer} seconds...`);
-    setTimeout(() => {
-      process.stdout.write(`\r\x07*BOOM* blasted off after ${timer} seconds!\n`);
-    }, timer * 1000);
-    setTimeout(() => {
-      askCountdown("Set a timer from 1 to 9: ");
-    }, (timer * 1000) + 3000);
-  }
+    } else {
+      console.log(`setting timer for ${timer} seconds...`);
+      setTimeout(() => {
+        process.stdout.write(`\r\x07*BOOM* blasted off after ${timer} seconds!\n`);
+      }, timer * 1000);
+      setTimeout(() => {
+        askCountdown("Set a timer from 1 to 9: ");
+      }, (timer * 1000) + 3000);
+    }
 };
 
 const askCountdown = (question) => {
   rl.question(question, answer => {
-    countdown(Number(answer));
+    countdown(answer);
   });
 };
 
